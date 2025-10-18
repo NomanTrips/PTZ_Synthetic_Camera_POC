2d3ffbe518e0709ada676c0d6778ca237dfc94b5
Brian Scott <brianscott0017@yahoo.com>
2025-10-18 17:23:21 -0400

    added src shim

--- rig_demo.py
+++ rig_demo.py
@@ -2,6 +2,9 @@
 
 from __future__ import annotations
 
+import os, sys
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
+
 import argparse
 from pathlib import Path
 from typing import Iterable, List
