49b8220f7575be1565524e69b1ce8c8760bacf15
Brian Scott <brianscott0017@yahoo.com>
2025-10-20 08:17:57 -0400

    Implement input handler and main loop orchestration

--- src/ptz_poc/__init__.py
+++ src/ptz_poc/__init__.py
@@ -1,6 +1,7 @@
 """PTZ Synthetic Camera proof-of-concept package."""
 
+from .input_handler import InputCommand, InputHandler
 from .rig import Rig, RigState
 from .video_reader import VideoReader
 
-__all__ = ["VideoReader", "Rig", "RigState"]
+__all__ = ["VideoReader", "Rig", "RigState", "InputHandler", "InputCommand"]
